48649b62359b9f43aed8953b96b89a0f13f5c688
Boyang Matsapola <boyangbass@gmail.com>
2019-09-06 17:57:12 -0500

    got the database to update

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
 require("dotenv").config();
-// inquirer
-//   .prompt({
-//     name: "item",
-//     type: "input",
-//     message: "What item would you like to buy?",
-//   })
-//   .then(answers => {
-//     // Use user feedback for... whatever!!
-//   });
+
+function updateProduct(quantity, id) {
+    console.log("Updating quantities...\n");
+    var query = connection.query(
+      "UPDATE products SET ? WHERE ?",
+      [
+        {
+          stock_quantity: quantity
+        },
+        {
+          id: id
+        }
+      ],
+      function(err, res) {
+        if (err) throw err;
+        console.log(res.affectedRows + " products updated!\n");
+        console.log(res)
+        
+        // Call deleteProduct AFTER the UPDATE completes
+        // deleteProduct();
+      }
+    );
+  
+    // logs the actual query being run
+    // console.log(query.sql);
+  }
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : process.env.password,
-  database : 'bamazon'
+    host     : 'localhost',
+    user     : 'root',
+    password : process.env.password,
+    database : 'bamazon'
 });
- 
+
 connection.connect(function(err) {
     if (err) {
         console.error('error connecting: ' + err.stack);
@@ -26,8 +43,37 @@ connection.connect(function(err) {
     
     console.log('connected as id ' + connection.threadId);
 });
+inquirer
+  .prompt([
+    {
+    name: "item",
+    type: "input",
+    message: "What item would you like to buy?",
+    },
+    {
+    name: "amount",
+    type: "input",
+    message: "Houw many units would you like to buy?",
+    },
+
+])
+  .then(answers => {
+    const amountOrder = answers.amount;
+    var sql = "SELECT * FROM ?? WHERE ?? = ?";
+    var inserts = ['products', 'product_name', answers.item];
+    sql = mysql.format(sql, inserts);
+      connection.query(sql, function (error, results, fields) {
+    if (error) throw error;
+    if(amountOrder <= results[0].stock_quantity){
+        const quantity = results[0].stock_quantity - amountOrder;
+        const id = results[0].ID;
+        console.log(results[0].ID)
+        updateProduct(quantity, id)
+
+    }
+    console.log(results[0].stock_quantity);
+    });
+
+   
+  });
 // connection.connect();
-// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-//   if (error) throw error;
-//   console.log('The solution is: ', results[0].solution);
-// });
